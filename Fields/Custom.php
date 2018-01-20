dea990ae0df78edc3b248d99f54589c4a07ca776
Domenico Giambra <sviluppo@editt.it>
2018-01-20 22:17:03 +0100

    Type hints and code reformatting

--- Fields/Custom.php
+++ Fields/Custom.php
@@ -2,24 +2,28 @@
 
 use Model\Form\MField;
 
-class Custom extends MField {
-	public function renderWithLang(array $attributes, $lang = null){
-		echo '<div data-custom="'.$this->options['name'].'">';
+class Custom extends MField
+{
+	public function renderWithLang(array $attributes, string $lang = null)
+	{
+		echo '<div data-custom="' . $this->options['name'] . '">';
 		echo $this->getText(['lang' => $lang]);
 		echo '</div>';
 	}
 
-	public function getText(array $options = []){
-		if(isset($this->options['custom'])){
-			if(!is_string($this->options['custom']) and is_callable($this->options['custom'])){
+	public function getText(array $options = []): string
+	{
+		if (isset($this->options['custom'])) {
+			if (!is_string($this->options['custom']) and is_callable($this->options['custom'])) {
 				return call_user_func($this->options['custom'], $this->form->options['element']);
-			}else{
+			} else {
 				return $this->options['custom'];
 			}
 		}
 	}
 
-	public function getValue($lang = null){
+	public function getValue($lang = null)
+	{
 		return $this->getText(['lang' => $lang]);
 	}
 }
